891f6132ada187a62157842d06e904fe79175578
SAK74 <sergijks@gmail.com>
2024-10-23 20:57:44 +0200

    refactor: refactorization

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import "dotenv/config";
-import { resolve } from "node:path";
 
-import aiClient from "./aiClient";
+import aiClient from "./src/aiClient";
 
 async function main() {
   try {
-    const res = await aiClient(resolve(__dirname, "./diff.txt"));
+    const res = await aiClient("./diff.txt");
     console.log("Result: ", res);
   } catch (error) {
     console.error("Error in main function:", error);
